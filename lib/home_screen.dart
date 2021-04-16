ccdba697437e81911d93cc6e38aa98675091ca1c
CarlHagred <carl.hagred@gmail.com>
2021-04-16 22:47:47 +0200

    Homepage list fixed and you can now add exercises

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,6 +13,54 @@ class HomeScreenPage extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreenPage> {
+  double topContainer = 0;
+  ScrollController controller = ScrollController();
+  List<Widget> itemsData = [];
+  void getPostsData() {
+    List<Widget> responseList = [];
+    for (var i = 0; i < lists.length; i++) {
+      responseList.add(
+        Container(
+          height: 50,
+          margin: EdgeInsets.all(2),
+          child: ElevatedButton(
+            child: Center(
+              child: Text(
+                '${lists.keys.elementAt(i)}',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.grey[800],
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ExercisePage(title: lists.keys.elementAt(i));
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      );
+    }
+    setState(() {
+      itemsData = responseList;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getPostsData();
+  }
+
   final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -25,8 +73,50 @@ class _HomeScreenState extends State<HomeScreenPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.all(15),
+              child: ListView(
+                children: itemsData,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Add Exercise'),
+                content: TextField(
+                  onChanged: (value) {},
+                  controller: myController,
+                  decoration: InputDecoration(
+                      hintText: "Type the name of the exercise"),
+                ),
+                actions: [
+                  TextButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      setState(() {
+                        lists['${myController.text}'] = [];
+                      });
+                      getPostsData();
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        tooltip: 'newExercise',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+/*
+padding: EdgeInsets.all(15),
                 itemCount: exercisesList.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
@@ -57,43 +147,4 @@ class _HomeScreenState extends State<HomeScreenPage> {
                         );
                       },
                     ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text('TextField in Dialog'),
-                content: TextField(
-                  onChanged: (value) {},
-                  controller: myController,
-                  decoration: InputDecoration(hintText: "Text Field in Dialog"),
-                ),
-                actions: [
-                  TextButton(
-                    child: Text('Add'),
-                    onPressed: () {
-                      setState(() {
-                        lists[myController.text.toString()] = [];
-                        Navigator.pop(context);
-                      });
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-        tooltip: 'newExercise',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+                  );*/
